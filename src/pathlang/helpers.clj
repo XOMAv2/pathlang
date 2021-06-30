e5f48e4b55ddd731c6a5ed1f169f0ff3d3320d9a
Nikita Iksaritsa <xomav2@gmail.com>
2021-07-01 00:49:35 +0300

    A function for printing a spec explanation

--- src/pathlang/helpers.clj
+++ src/pathlang/helpers.clj
@@ -1,6 +1,21 @@
-(ns pathlang.helpers)
+(ns pathlang.helpers
+  (:require [expound.alpha :as ex]))
 
 (defn not-coll?
   "Returns true if x does not implements IPersistentCollection."
   [x]
   (not (coll? x)))
+
+(defn map-value [f map]
+  (reduce-kv (fn [acc k v]
+               (assoc acc k (f v)))
+             {} map))
+
+(defn beautiful-spec-explain
+  "Just a wrapper over the expound-str function of
+   the expound library with several keys."
+  [spec val]
+  (ex/expound-str spec val
+                  {:print-specs? false
+                   :show-valid-values? true}))
+
